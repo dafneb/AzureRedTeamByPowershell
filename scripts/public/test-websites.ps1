267f2a588e08c9a35bf7924805f0d7160954c375
David Burel <david.burel@burelovi.eu>
2025-04-19 15:24:38 +0200

    Refactor website checking script: update output messages, improve logging, and enhance error handling

--- scripts/public/test-websites.ps1
+++ scripts/public/test-websites.ps1
@@ -28,7 +28,7 @@ param (
 $timeStart = Get-Date
 
 Write-Output "***********************************************************"
-Write-Output "*********** Websites mining *******************************"
+Write-Output "*********** Websites scraping *****************************"
 Write-Output "*********** Author: David Burel (@dafneb) *****************"
 Write-Output "***********************************************************"
 
@@ -46,11 +46,12 @@ $caseFolderName = $CaseName.ToLower()
 $caseFolderName = $caseFolderName.Trim()
 $caseFolderName = $caseFolderName -replace '[\\/:*?"<>|]', '_'
 
-# Paths for logs (1/2)
+# Paths for logs and case folders
 $baseFolderPath = Join-Path -Path (Get-Location) -ChildPath "case"
 $caseFolderPath = Join-Path -Path $baseFolderPath -ChildPath "$($caseFolderName)"
+$storFilePath = Join-Path -Path $caseFolderPath -ChildPath "storageaccounts.csv"
 
-Write-Verbose -Message "Checking folders (1/2) ..."
+Write-Verbose -Message "Checking folders ..."
 
 # Create case folder if it doesn't exist
 if (-not (Test-Path -Path $baseFolderPath)) {
@@ -64,32 +65,102 @@ if (-not (Test-Path -Path $caseFolderPath)) {
     New-Item -ItemType Directory -Path $caseFolderPath | Out-Null
 }
 
+# Create log file if it doesn't exist
+if (-not (Test-Path -Path $storFilePath)) {
+    Write-Verbose -Message "Log file does not exist, creating it..."
+    New-Item -ItemType File -Path $storFilePath | Out-Null
+} else {
+    Clear-Content -Path $storFilePath
+}
+
+# Prepare list of websites to check
 $websites = @()
 switch ($PSCmdlet.ParameterSetName) {
     'Uri' {
+        # Get websites from the command line
+        Write-Verbose -Message "Reading websites from command line ..."
         $websites += $Uri
-        Write-Verbose -Message "Websites to check: $($websites)"
     }
 
     'File' {
+        # Get websites from a file
         Write-Verbose -Message "Reading websites from file $($FilePath) ..."
         if (Test-Path -Path $FilePath -PathType Leaf) {
             $websites += Get-Content -Path $FilePath
-            Write-Verbose -Message "Websites to check: $($websites)"
         } else {
             Write-Error -Message "File not found: $($FilePath)" -Category ObjectNotFound
             exit
         }
     }
 }
+Write-Verbose -Message "Websites to check: $($websites)"
 
+# Let's check the websites ....
+Write-Verbose -Message "Checking websites ..."
+$dataStorages = @()
+$websites | ForEach-Object {
+    $address = $_.Trim()
+    Write-Verbose -Message "Processing address: $($address)"
+    try {
+        $websiteUri = New-Object System.UriBuilder($address)
+    } catch {
+        Write-Warning -Message "Error processing address: $($address)"
+        continue
+    }
 
+    # Let's check if the website is accessible
+    try {
+        $response = Invoke-WebRequest -Uri $websiteUri.Uri
+        Write-Verbose -Message "StatusCode: $($response.StatusCode)"
+        Write-Verbose -Message "StatusDescription: $($response.StatusDescription)"
+    } catch {
+        Write-Warning -Message "Failed to access the website: $($address)"
+        Write-Warning -Message "StatusCode: $($_.Exception.Response.StatusCode.value__)"
+        continue
+    }
 
+    # Check headers 
+    Write-Verbose -Message "Headers:"
+    $response.Headers.Keys | ForEach-Object {
+        $key = $_
+        Write-Verbose -Message "$($key): $($response.Headers[$key] -Join ', ')"
+    }
 
+    # Check content and try find if it's a blob storage
+    Write-Verbose -Message "Checking for blob storage ..."
+    if ($response.StatusCode -eq 200) {
+        # Check if the website contains any link to a blob storage
+        # Pattern for all services under StorageAccount 'https://([0-9a-z]{3,24})\.(blob|web|dfs|file|queue|table)\.core\.windows\.net/([0-9a-z-_$]{3,63})/'
+        Write-Verbose -Message "Checking for regular expression against context:"    
+        Write-Verbose -Message "Pattern: (?<endpoint>https://(?<storageacc>[0-9a-z]{3,24})\.blob\.core\.windows\.net)/(?<container>[0-9a-z-_$]{3,63})/"    
+        $results = $response.Content | Select-String -Pattern '(?<endpoint>https://(?<storageacc>[0-9a-z]{3,24})\.blob\.core\.windows\.net)/(?<container>[0-9a-z-_$]{3,63})/' -AllMatches
+        # We want to get unique matches
+        $uniqueMatches = $results.Matches | Select-Object -Unique
+        Write-Verbose -Message "Unique Matches:"
+        $uniqueMatches | ForEach-Object {
+            Write-Verbose -Message "Value: $($_.Value)"
+            Write-Verbose -Message "Endpoint: $($_.Groups['endpoint'].Value)"
+            Write-Verbose -Message "StorageAccount: $($_.Groups['storageacc'].Value)"
+            Write-Verbose -Message "Container: $($_.Groups['container'].Value)"
+            $dataStorages += [PSCustomObject]@{
+                Value = "$($_.Value)"; 
+                Endpoint = "$($_.Groups['endpoint'].Value)"; 
+                StorageAccount = "$($_.Groups['storageacc'].Value)"; 
+                Container = "$($_.Groups['container'].Value)" 
+            }
+        }
 
+    } else {
+        Write-Warning -Message "Failed to access the website: $($address)"
+        Write-Warning -Message "StatusCode: $($response.StatusCode)"
+        continue
+    }
 
+}
 
-
+# Export the storage account details to a CSV file
+Write-Verbose -Message "Saving data ..."
+$dataStorages | Export-Csv -Path $storFilePath -NoTypeInformation
 
 # Get actual date and time ...
 $timeEnd = Get-Date
